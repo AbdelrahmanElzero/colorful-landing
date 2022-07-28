5f2c982b505a37ff6b1a930417db8ca06726807d
AbdelrahmanElzero <zerosc201@gmail.com>
2022-07-28 14:16:10 +0200

    created coloful pepsi landing

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img")
+let container = document.querySelector('.container')
+
+function cans(can){
+  img.src = can;
+}
+
+function background(color){
+  container.style.background = color;
+}
